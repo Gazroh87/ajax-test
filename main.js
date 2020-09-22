62d3eb2c7eca976b228c9bfca579b0f5e3423690
Gary Hughes <gary.hughes87@virginmedia.com>
2020-09-22 09:48:36 +0000

    Added additional javascript in main.js to tabularise data

--- main.js
+++ main.js
@@ -1,27 +1,77 @@
-let xhr = new XMLHttpRequest();
-let data;
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-xhr.send();
+function getData(type, cb) {
+  let xhr = new XMLHttpRequest();
+  // let data;
 
-// function setData(jsonData) {
-   // data = jsonData;
-   // console.log(data);
-// }
+  xhr.open("GET", baseURL + type + "/");
+  xhr.send();
 
-xhr.onreadystatechange = function () {
-   // console.log(this.readyState);
-  if (this.readyState == 4 && this.status == 200) {
-    // document.getElementById("data").innerHTML = this.responseText;
-    // console.log(typeof (JSON.parse(this.responseText)));
-    // console.log(JSON.parse(this.responseText));
-    // data = this.responseText;
-    // console.log(data);
-    // setData(JSON.parse(this.responseText));
-    data = JSON.parse(this.responseText);
-  }
-};
+  /* function setData(jsonData) {
+   data = jsonData;
+   console.log(data);
+   } */
 
-setTimeout(function() {
-    console.log(data);
-}, 500);
+  xhr.onreadystatechange = function () {
+    console.log(this.readyState);
+    if (this.readyState == 4 && this.status == 200) {
+      /* document.getElementById("data").innerHTML = this.responseText;
+       console.log(typeof (JSON.parse(this.responseText)));
+       console.log(JSON.parse(this.responseText));
+       data = this.responseText;
+       console.log(data);
+       setData(JSON.parse(this.responseText));
+       data = JSON.parse(this.responseText); */
+      cb(JSON.parse(this.responseText));
+    }
+  };
+}
+
+function getTableHeaders(obj) {
+  let tableHeaders = [];
+
+  Object.keys(obj).forEach(function (key) {
+    tableHeaders.push(`<td>${key}</td>`);
+  });
+
+  return `<tr>${tableHeaders}</tr>`;
+}
+
+function writeToDocument(type) {
+  let tableRows = [];
+  let el = document.getElementById("data");
+  el.innerHTML = "";
+  getData(type, function (data) {
+    // console.dir(data);
+    data = data.results;
+    let tableHeaders = getTableHeaders(data[0]);
+
+    data.forEach(function (item) {
+      // el.innerHTML += "<p>" + item.name + "</p>";
+      let dataRow = [];
+
+      Object.keys(item).forEach(function (key) {
+        let rowData = item[key].toString();
+        let truncatedData = rowData.substring(0, 15);
+        dataRow.push(`<td>${truncatedData}</td>`);
+      });
+      tableRows.push(`<tr>${dataRow}</tr>`);
+    });
+
+    el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+  });
+}
+
+/* setTimeout(function () {
+ console.log(data);
+ }, 500);
+
+ function printDataToConsole(data) {
+ console.log(data);
+ }
+
+ getData(printDataToConsole);
+
+ getData(function (data) {
+console.log(data);
+}); */
